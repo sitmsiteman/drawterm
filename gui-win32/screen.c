504cca1cfbbe7d0d3e912ddcd658af963ed78380
sitmsiteman <sitmsiteman@gmail.com>
2026-02-02 14:11:14 +0900

    win32: fix Alt+Tab activating compose mode
    When using Alt+Tab to switch from the drawterm window, the Alt key
    activates the compose mode.  This causes subsequent inputs to be
    registered in compose mode, which is not desired.  This patch fixes
    the issue by clearing scdown and sending a dummy input (Kctl) to
    cancel the compose mode when the window loses focus via Alt+Tab.  Kctl
    is chosen because it seems to be the most harmless input to cancel the
    compose mode.

--- gui-win32/screen.c
+++ gui-win32/screen.c
@@ -436,6 +436,20 @@ WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 		}
 		break;
 
+	case WM_KILLFOCUS:
+	case WM_ENTERSIZEMOVE:
+		for(i = 0; i < 256; i++){
+			if(scdown[i] != 0){
+				kbdkey(scdown[i], 0);
+				if(scdown[i] == Kalt){
+					kbdkey(Kctl, 1); /* Dummy input */
+					kbdkey(Kctl, 0);
+				}
+				scdown[i] = 0;
+			}
+		}
+		break;
+
 	case WM_CLOSE:
 		DestroyWindow(hwnd);
 		break;
